1b166251a574e9f34905f12fdd6d329aa3df9dc0
Renaud Tahon <renaud.tahon@std.heh.be>
2017-10-19 11:35:26 +0200

    DefaultProject-spring

---
+++ vetapp/src/main/java/be/heh/teamsimond/vetapp/VetappApplication.java
@@ -0,0 +1,12 @@
+package be.heh.teamsimond.vetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetappApplication.class, args);
+	}
+}
