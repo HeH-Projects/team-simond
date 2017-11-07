476748b6ff10d028092c62e6e1e48986eb115ca2
Renaud Tahon <renaud.tahon@std.heh.be>
2017-11-07 09:06:05 +0100

    Merge doctor and doctor_time_slots
    Parce que Victor

--- vetapp/src/main/java/be/heh/teamsimond/vetapp/JPA/Doctor.java
+++ vetapp/src/main/java/be/heh/teamsimond/vetapp/JPA/Doctor.java
@@ -16,12 +16,27 @@ public class Doctor extends VetappElement{
     @Column(name="default_room_id")
     private int roomId;
 
+    private String monday;
+    private String tuesday;
+    private String wednesday;
+    private String thirsday;
+    private String friday;
+    private String saturday;
+    private String sunday;
+
     public Doctor(){}
 
-    public Doctor(int id, String name, int roomId){
+    public Doctor(int id, String name, int roomId, String monday, String tuesday, String wednesday, String thirsday, String friday, String saturday, String sunday){
         this.id = id;
         this.name = name;
         this.roomId = roomId;
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thirsday = thirsday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
     }
 
     public int getId() {
@@ -46,5 +61,61 @@ public class Doctor extends VetappElement{
 
     public void setRoomId(int roomId){
         this.roomId = roomId;
+    }
+
+    public String getMonday() {
+        return monday;
+    }
+
+    public void setMonday(String monday) {
+        this.monday = monday;
+    }
+
+    public String getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(String tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public String getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(String wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public String getThirsday() {
+        return thirsday;
+    }
+
+    public void setThirsday(String thirsday) {
+        this.thirsday = thirsday;
+    }
+
+    public String getFriday() {
+        return friday;
+    }
+
+    public void setFriday(String friday) {
+        this.friday = friday;
+    }
+
+    public String getSaturday() {
+        return saturday;
+    }
+
+    public void setSaturday(String saturday) {
+        this.saturday = saturday;
+    }
+
+    public String getSunday() {
+        return sunday;
+    }
+
+    public void setSunday(String sunday) {
+        this.sunday = sunday;
     }
 }
