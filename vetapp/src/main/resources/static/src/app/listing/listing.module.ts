58f4a9bc84ffdf24d60a66dc26e50144eb518559
Florian Simond <simond.florian@gmail.com>
2017-11-08 15:02:11 +0100

    Angular page navigation update

--- vetapp/src/main/resources/static/src/app/listing/listing.module.ts
+++ vetapp/src/main/resources/static/src/app/listing/listing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { UsersComponent } from './users/users.component'
 import { DoctorComponent } from './doctors/doctor.component';
-import { ClientComponent } from './clients/client.component';
 import { RoomComponent } from './rooms/room.component';
+import { ClientComponent } from './clients/client.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-        { path: 'doctors', component: DoctorComponent },
-        { path: 'clients', component: ClientComponent },
-        { path: 'rooms', component: RoomComponent }
+        { path: 'users', component: UsersComponent },
+        { path: 'users/doctors', component: DoctorComponent },
+        { path: 'users/clients', component: ClientComponent },
+        { path: 'users/rooms', component: RoomComponent },
     ])
   ],
   declarations: [
+      UsersComponent,
       DoctorComponent,
       ClientComponent,
       RoomComponent
