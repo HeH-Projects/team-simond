69960a653d53f391ef63a1b2aa5b3196b6f87786
Unknown <github@victor.taleb.me>
2017-11-29 11:31:15 +0100

    org.hibernate.Query has been deprecated

--- vetapp/src/main/java/be/heh/teamsimond/vetapp/VetappElementRepositoryHibernate.java
+++ vetapp/src/main/java/be/heh/teamsimond/vetapp/VetappElementRepositoryHibernate.java
@@ -1,13 +1,11 @@
 package be.heh.teamsimond.vetapp;
 
-import be.heh.teamsimond.vetapp.JPA.Appointment;
 import be.heh.teamsimond.vetapp.JPA.Customer;
-import be.heh.teamsimond.vetapp.JPA.Doctor;
 import be.heh.teamsimond.vetapp.JPA.Patient;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.TypedQuery;
 import java.util.Date;
 import java.util.List;
 
@@ -56,26 +54,26 @@ public class VetappElementRepositoryHibernate implements IVetappElementRepositor
 
     public List<IVetappElement> findById(Class c, int id) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM "+c.getName()+" c WHERE c.id = :id");
+        TypedQuery<IVetappElement> query = session.createQuery("FROM "+c.getName()+" c WHERE c.id = :id");
         query.setParameter( "id", id);
-        List<IVetappElement> list = query.list();
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
 
     public List<IVetappElement> findByIncompleteName(Class c, String str) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM "+c.getName()+" c WHERE c.name LIKE :str");
+        TypedQuery<IVetappElement> query = session.createQuery("FROM "+c.getName()+" c WHERE c.name LIKE :str");
         query.setParameter( "str", str + "%");
-        List<IVetappElement> list = query.list();
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
 
     public List<IVetappElement> findAll(Class c) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM "+c.getName()+" c");
-        List<IVetappElement> list = query.list();
+        TypedQuery<IVetappElement> query = session.createQuery("FROM "+c.getName()+" c");
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
@@ -85,19 +83,19 @@ public class VetappElementRepositoryHibernate implements IVetappElementRepositor
     }
     public List<IVetappElement> findPatientsByCustomer(int customerId) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM Patient c WHERE c.customerId = :customer_id");
+        TypedQuery<IVetappElement> query = session.createQuery("FROM Patient c WHERE c.customerId = :customer_id");
         query.setParameter( "customer_id", customerId);
-        List<IVetappElement> list = query.list();
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
 
     public List<IVetappElement> findAppointmentsInInterval(Date start, Date end) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM Appointment c WHERE c.id.date BETWEEN :start AND :end");
+        TypedQuery<IVetappElement> query = session.createQuery("FROM Appointment c WHERE c.id.date BETWEEN :start AND :end");
         query.setParameter( "start", start);
         query.setParameter("end", end);
-        List<IVetappElement> list = query.list();
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
@@ -107,10 +105,10 @@ public class VetappElementRepositoryHibernate implements IVetappElementRepositor
     }
     public List<IVetappElement> findAppointmentByDate_Patient(Date date, int patientId) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM Appointment c WHERE c.id.date = :date AND c.id.patientId = :patient_id");
+        TypedQuery<IVetappElement> query = session.createQuery("FROM Appointment c WHERE c.id.date = :date AND c.id.patientId = :patient_id");
         query.setParameter( "date", date);
         query.setParameter("patient_id", patientId);
-        List<IVetappElement> list = query.list();
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
@@ -120,9 +118,9 @@ public class VetappElementRepositoryHibernate implements IVetappElementRepositor
     }
     public List<IVetappElement> findAppointmentsByPatient(int patientId) {
         Session session = this.s_beginTransaction();
-        Query query = session.createQuery("FROM Appointment c WHERE c.id.patientId = :patient_id");
+        TypedQuery<IVetappElement> query = session.createQuery("FROM Appointment c WHERE c.id.patientId = :patient_id");
         query.setParameter("patient_id", patientId);
-        List<IVetappElement> list = query.list();
+        List<IVetappElement> list = query.getResultList();
         this.s_commitClose(session);
         return list;
     }
